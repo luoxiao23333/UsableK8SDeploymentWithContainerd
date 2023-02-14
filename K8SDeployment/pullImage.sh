b7d4a0c23cfa7e249ccee6d3bc0b3dca06a32b2d
罗霄 <2362844711@qq.com>
2023-02-14 10:47:54 +0600

    change kubeadm config

--- K8SDeployment/pullImage.sh
+++ K8SDeployment/pullImage.sh
@@ -1,23 +1,23 @@
-docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver:v1.25.2
-docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver:v1.25.2 registry.k8s.io/kube-apiserver:v1.25.2
+ctr i pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver:v1.26.1
+ctr i tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver:v1.26.1 registry.k8s.io/kube-apiserver:v1.26.1
 
-docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager:v1.25.2
-docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager:v1.25.2 registry.k8s.io/kube-controller-manager:v1.25.2
+ctr i pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager:v1.26.1
+ctr i tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager:v1.26.1 registry.k8s.io/kube-controller-manager:v1.26.1
 
-docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-scheduler:v1.25.2
-docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-scheduler:v1.25.2 registry.k8s.io/kube-scheduler:v1.25.2
+ctr i pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-scheduler:v1.26.1
+ctr i tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-scheduler:v1.26.1 registry.k8s.io/kube-scheduler:v1.26.1
 
-docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy:v1.25.2
-docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy:v1.25.2 registry.k8s.io/kube-proxy:v1.25.2
+ctr i pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy:v1.26.1
+ctr i tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy:v1.26.1 registry.k8s.io/kube-proxy:v1.26.1
 
-docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/pause:3.8
-docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/pause:3.8 registry.k8s.io/pause:3.8
+ctr i pull registry.cn-hangzhou.aliyuncs.com/google_containers/pause:3.9
+ctr i tag registry.cn-hangzhou.aliyuncs.com/google_containers/pause:3.9 registry.k8s.io/pause:3.9
 
-docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/etcd:3.5.4-0
-docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/etcd:3.5.4-0 registry.k8s.io/etcd:3.5.4-0
+ctr i pull registry.cn-hangzhou.aliyuncs.com/google_containers/etcd:3.5.6-0
+ctr i tag registry.cn-hangzhou.aliyuncs.com/google_containers/etcd:3.5.6-0 registry.k8s.io/etcd:3.5.6-0
 
-docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:v1.9.3
-docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:v1.9.3 registry.k8s.io/coredns/coredns:v1.9.3
+ctr i pull registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:v1.9.3
+ctr i tag registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:v1.9.3 registry.k8s.io/coredns/coredns:v1.9.3
 
 
 
